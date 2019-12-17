e55e914ed521126cb43e0097e2fc592dfc82c4dd
BMWeygant <bobbydigital108@gmail.com>
2019-12-17 12:41:27 -0500

    Added comments and optimized solutions in Solution branch.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const add = (a, b) => a + b
-
-const sub = (a, b) => a - b
-
-// const total = (subTotal, shipping) => { 
-//     return '$'+ add(subTotal, shipping)
-// }
-
-function total(subTotal, shipping){
-    return '$' + add(subTotal, shipping)
+//Solution using non ES6 Javascript.
+function add(a, x){
+    return a + x
 }
+//Solution using ES6 Javascript 
+//This highlights test cases ability to read different solutions to the same problem.
+const sub = (x, y) => x - y
 
-function stringTheory() {
-return "Some wild shit!"
-}
 
+//Solution to a test case written to test basic booleans in an object
 const myBeverage = {
     delicious: true,
     sour: false,
-    full:true
+    full: true
 }
+
+//Solution to test that requires another predefined function to be used within this function
+const total = (subTotal, shipping) => {
+    newTotal = add(subTotal, shipping)
+       return '$' + `${newTotal}`
+}
+/* return '$' + `${add(subTotal + shipping)}` also satisfies test requirement
+However, setting solution to a variable is good practice
+Just in case you wanted to implement more functions within total
+For example a tax function.*/
+
+
+//ES6 style solution to test a function that returns a specific string
+const stringTheory = () => "Some wild shit!"
 
 module.exports = {add, sub, total, stringTheory, myBeverage}
